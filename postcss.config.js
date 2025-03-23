df33fcd4f28d4461d962943d014e34cc2e172d4d
Sunitha <sunithab188@gmail.com>
2025-03-23 23:29:22 +0530

    Tailwind css production update

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      // Production only
-      ...(process.env.NODE_ENV === 'production' ? [require('@fullhuman/postcss-purgecss')({
-        content: ['./src//.html', './src//.jsx', './src//*.js'],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      }), require('cssnano')]:[]),
-  ]
-  }
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
